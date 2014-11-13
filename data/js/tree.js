cd02603d4534ee7c776624d3a54ec945ebf2b300
Sherman Chen <shamen.chens@gmail.com>
2014-11-14 00:08:50 +0800

    Add tree model and node

--- data/js/tree.js
+++ data/js/tree.js
@@ -1,5 +1,207 @@
 'use strict';
 (function(exports) {
-  function TreeManager() {
+  var walkStrategies = {};
+
+  function TreeManager() {}
+
+  TreeManager.prototype.parse = function (model) {
+    var i, childCount, node;
+
+    if (!(model instanceof Object)) {
+      throw new TypeError('Model must be of type object.');
+    }
+
+    node = new Node(this.config, model);
+    if (model['children'] instanceof Array) {
+      for (i = 0, childCount = model['children'].length; i < childCount; i++) {
+        addChildToNode(node, this.parse(model['children'][i]));
+      }
+    }
+    return node;
+  };
+
+  function addChildToNode(node, child) {
+    child.parent = node;
+    node.children.push(child);
+    return child;
   }
+
+  function Node(nameIdx, options) {
+    this.nameIdx = nameIdx;
+    this.selfAccu = options.selfAccu;
+    this.selfSize = options.selfSize;
+    this.selfHWM = options.selfHWM;
+    this.totalAccu = options.totalAccu;
+    this.totalSize = options.totalSize;
+    this.totalHWM = options.totalHWM;
+    this.children = [];
+  }
+
+  Node.prototype.isRoot = function n_isRoot() {
+    return this.parent === undefined;
+  };
+
+  Node.prototype.hasChildren = function n_hasChildren() {
+    return this.children.length > 0;
+  };
+
+  Node.prototype.addChild = function n_addChild(child) {
+    child.parent = this;
+    this.model['children'].push(child.model);
+    this.children.push(child);
+    return child;
+  };
+
+  Node.prototype.getPath = function () {
+    var path = [];
+    (function addToPath(node) {
+      path.unshift(node);
+      if (!node.isRoot()) {
+        addToPath(node.parent);
+      }
+    })(this);
+    return path;
+  };
+
+  Node.prototype.update = function n_update(options) {
+    if (options.selfAccu) {
+      this.selfAccu += options.selfAccu;
+    }
+    if (options.selfSize) {
+      this.selfSize += options.selfSize;
+    }
+    if (options.selfHWM) {
+      this.selfHWM += options.selfHWM;
+    }
+    if (options.totalAccu) {
+      this.totalAccu += options.totalAccu;
+    }
+    if (options.totalSize) {
+      this.totalSize += options.totalSize;
+    }
+    if (options.totalHWM) {
+      this.totalHWM += options.totalHWM;
+    }
+  };
+
+  /**
+   * Parse the arguments of traversal functions. These functions can take one optional
+   * first argument which is an options object. If present, this object will be stored
+   * in args.options. The only mandatory argument is the callback function which can
+   * appear in the first or second position (if an options object is given). This
+   * function will be saved to args.fn. The last optional argument is the context on
+   * which the callback function will be called. It will be available in args.ctx.
+   *
+   * @returns Parsed arguments.
+   */
+  function parseArgs() {
+    var args = {};
+    if (arguments.length === 1) {
+      args.fn = arguments[0];
+    } else if (arguments.length === 2) {
+      if (typeof arguments[0] === 'function') {
+        args.fn = arguments[0];
+        args.ctx = arguments[1];
+      } else {
+        args.options = arguments[0];
+        args.fn = arguments[1];
+      }
+    } else {
+      args.options = arguments[0];
+      args.fn = arguments[1];
+      args.ctx = arguments[2];
+    }
+    args.options = args.options || {};
+    if (!args.options.strategy) {
+      args.options.strategy = 'pre';
+    }
+    if (!walkStrategies[args.options.strategy]) {
+      throw new Error('Unknown tree walk strategy. Valid strategies are \'pre\' [default], \'post\' and \'breadth\'.');
+    }
+    return args;
+  }
+
+  Node.prototype.walk = function () {
+    var args;
+    args = parseArgs.apply(this, arguments);
+    walkStrategies[args.options.strategy].call(this, args.fn, args.ctx);
+  };
+
+  walkStrategies.pre = function depthFirstPreOrder(callback, context) {
+    var i, childCount, keepGoing;
+    keepGoing = callback.call(context, this);
+    for (i = 0, childCount = this.children.length; i < childCount; i++) {
+      if (keepGoing === false) {
+        return false;
+      }
+      keepGoing = depthFirstPreOrder.call(this.children[i], callback, context);
+    }
+    return keepGoing;
+  };
+
+  walkStrategies.post = function depthFirstPostOrder(callback, context) {
+    var i, childCount, keepGoing;
+    for (i = 0, childCount = this.children.length; i < childCount; i++) {
+      keepGoing = depthFirstPostOrder.call(this.children[i], callback, context);
+      if (keepGoing === false) {
+        return false;
+      }
+    }
+    keepGoing = callback.call(context, this);
+    return keepGoing;
+  };
+
+  walkStrategies.breadth = function breadthFirst(callback, context) {
+    var queue = [this];
+    (function processQueue() {
+      var i, childCount, node;
+      if (queue.length === 0) {
+        return;
+      }
+      node = queue.shift();
+      for (i = 0, childCount = node.children.length; i < childCount; i++) {
+        queue.push(node.children[i]);
+      }
+      if (callback.call(context, node) !== false) {
+        processQueue();
+      }
+    })();
+  };
+
+  Node.prototype.all = function () {
+    var args, all = [];
+    args = parseArgs.apply(this, arguments);
+    walkStrategies[args.options.strategy].call(this, function (node) {
+      if (args.fn.call(args.ctx, node)) {
+        all.push(node);
+      }
+    }, args.ctx);
+    return all;
+  };
+
+  Node.prototype.first = function () {
+    var args, first;
+    args = parseArgs.apply(this, arguments);
+    walkStrategies[args.options.strategy].call(this, function (node) {
+      if (args.fn.call(args.ctx, node)) {
+        first = node;
+        return false;
+      }
+    }, args.ctx);
+    return first;
+  };
+
+  Node.prototype.drop = function () {
+    var indexOfChild;
+    if (!this.isRoot()) {
+      indexOfChild = this.parent.children.indexOf(this);
+      this.parent.children.splice(indexOfChild, 1);
+      this.parent.model['children'].splice(indexOfChild, 1);
+      this.parent = undefined;
+      delete this.parent;
+    }
+    return this;
+  };
+
+  exports.TreeManager = TreeManager;
 }(window));
